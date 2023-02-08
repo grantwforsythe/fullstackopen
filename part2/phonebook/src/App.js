a0414ebf4562176dfb89dcf03b55c81a5c6015e2
Grant Forsythe <grantwforsythe@gmail.com>
2023-02-07 22:13:54 -0500

    Add useEffect to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 import Filter from './components/Filter';
@@ -6,15 +7,16 @@ import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 } 
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data));
+  }, []);
 
   const handleFilter = (event) => {
     console.log('filter: ', event.target.value);
@@ -50,8 +52,6 @@ const App = () => {
         number: newPhone
       }));
     }
-
-    console.log(persons.length);
 
     setNewName('');
     setNewPhone('');
