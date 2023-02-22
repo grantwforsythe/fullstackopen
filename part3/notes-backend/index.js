462aa9ecd0efe8eda6100e6fb7540506b7c66a29
Grant Forsythe <grantwforsythe@gmail.com>
2023-02-21 21:01:50 -0500

    Add notes-backend

---
+++ part3/notes-backend/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+
+// Parse request bodies in JSON format
+app.use(express.json());
+
+let notes = [
+  {
+    content: "HTML is easy",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: false,
+    id: 2,
+  },
+  {
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+    id: 3,
+  }
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0;
+    return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  if (!request.body.content) {
+    return response.status(400).json({ error: 'Content missing' });
+  }
+
+  const note = {
+    content: request.body.content,
+    important: request.body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).json({
+      error: `No note found with an id of ${id}`,
+      errorCode: 404
+    });
+  }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end()
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Live on http://localhost:${PORT}`);
+});
