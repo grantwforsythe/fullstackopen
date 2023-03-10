94a612b329203ae2a9744fe45ee9b4ed4f258347
Grant Forsythe <grantwforsythe@gmail.com>
2023-03-10 15:10:26 -0500

    Refactor part4/notes to use 'express-async-errors' package

--- part4/notes/backend/routes/api.js
+++ part4/notes/backend/routes/api.js
@@ -3,64 +3,47 @@ const router = express.Router();
 
 const Note = require('../models/note');
 
-router.get('/notes', async (request, response) => {
+router.get('', async (request, response) => {
   const notes = await Note.find({});
   response.json(notes);
 });
 
-router.post('/notes', async (request, response, next) => {
+router.post('', async (request, response) => {
   const note = new Note({
     content: request.body.content,
     important: request.body.important || false,
   });
 
-  try {
-    await note.save();
+  await note.save();
+  response.status(201).json(note);
+});
+
+router.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id);
+
+  if (note) {
     response.json(note);
-  } catch (error) {
-    // ValidationError: content property was not set in the request
-    next(error);
+  } else {
+    response.status(404).end();
   }
 });
 
-router.get('/notes/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
+router.put('/:id', async (request, response) => {
+  const note = await Note.findByIdAndUpdate(
+    request.params.id,
+    { important: request.body.important },
+    {
+      new: true, // Return the document after it's been updated
+      runValidators: true, // Validate on update
+      context: 'query',
     }
-  } catch (error) {
-    next(error);
-  }
+  );
+  response.json(note);
 });
 
-router.put('/notes/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findByIdAndUpdate(
-      request.params.id,
-      { important: request.body.important },
-      {
-        new: true,            // Return the document after it's been updated
-        runValidators: true,  // Validate on update
-        context: 'query',
-      }
-    );
-    response.json(note);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/notes/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id);
-    response.status(204).end();
-  } catch (error) {
-    next(error);
-  }
+router.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = router;
