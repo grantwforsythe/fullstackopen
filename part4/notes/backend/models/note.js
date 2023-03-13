9becdfeb88386f71ffa8d69c93fa8fdba51ba419
Grant Forsythe <grantwforsythe@gmail.com>
2023-03-13 16:58:25 -0400

    Add createdAt and updatedAt fields to noteSchema

--- part4/notes/backend/models/note.js
+++ part4/notes/backend/models/note.js
@@ -22,6 +22,20 @@ const noteSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
+
+noteSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
 });
 
 noteSchema.set('toJSON', {
