b19dae7c1ea9baae794a1b797c1fe0039f6493c8
Grant Forsythe <grantwforsythe@gmail.com>
2023-04-13 09:07:17 -0400

    Update validation error  messages

--- part4/blog/backend/models/user.js
+++ part4/blog/backend/models/user.js
@@ -11,15 +11,15 @@ mongoose.connect(config.MONGODB_URI);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    minLength: 3,
-    maxLength: 16,
+    minLength: [3, 'Username must be at least 3 characters, got {VALUE}'],
+    maxLength: [16, 'Username must be no more than 16 characters'],
     unique: true,
     required: true,
   },
   name: String,
   passwordHash: {
     type: String,
-    minLength: 3,
+    minLength: [3, 'Must be at least of length 3, got {VALUE}'],
     required: true,
   },
   blogs: [
@@ -30,7 +30,9 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator, {
+  message: 'Expected username to be unique',
+});
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
