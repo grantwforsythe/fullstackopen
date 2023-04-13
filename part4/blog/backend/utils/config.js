daadc0bb56a90f180776c08ad8dc514c126a1735
Grant Forsythe <grantwforsythe@gmail.com>
2023-04-13 08:31:49 -0400

    Add cross-env as a dev dependencies

--- part4/blog/backend/utils/config.js
+++ part4/blog/backend/utils/config.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
 
-const { PORT, MONGODB_URI } = process.env;
+const getURI = () => {
+  switch (process.env.NODE_ENV) {
+    case 'prod':
+      return process.env.MONGODB_URI;
+    case 'dev':
+      return process.env.DEV_MONGODB_URI;
+    case 'test':
+      return process.env.TEST_MONGODB_URI;
+    default:
+      return process.env.DEV_MONGODB_URI;
+  }
+};
+
+const { PORT } = process.env;
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS, 10);
+
+const MONGODB_URI = getURI();
 
 module.exports = {
   PORT,
