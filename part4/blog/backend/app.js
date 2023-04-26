81ee63130b1507fdbafe9bfbdb51b7432ab64b08
Grant Forsythe <grantwforsythe@gmail.com>
2023-04-25 22:14:22 -0400

    Move expressjwt middleware to only blog route

--- part4/blog/backend/app.js
+++ part4/blog/backend/app.js
@@ -16,11 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/login', loginRouter);
-
 // Authorization: populates the request.auth object
-app.use(expressjwt({ secret: config.ACCESS_TOKEN, algorithms: ['HS256'] }));
-
-app.use('/api/blogs', blogsRouter);
+app.use(
+  '/api/blogs',
+  expressjwt({ secret: config.ACCESS_TOKEN, algorithms: ['HS256'] }),
+  blogsRouter
+);
 app.use('/api/users', usersRouter);
 
 app.use(errorHandler);
