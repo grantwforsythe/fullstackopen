ab994eb6a18c98837d56d4bc804b0f7e098c671f
Grant Forsythe <grantwforsythe@gmail.com>
2023-05-02 08:47:55 -0400

    Remove express-jwt

--- part4/blog/backend/app.js
+++ part4/blog/backend/app.js
@@ -1,27 +1,24 @@
 const express = require('express');
 require('express-async-errors');
-const { expressjwt } = require('express-jwt');
+
 const cors = require('cors');
-const config = require('./utils/config');
+const cookieParser = require('cookie-parser');
 
 const blogsRouter = require('./routes/blogs');
 const usersRouter = require('./routes/user');
 const loginRouter = require('./routes/login');
 
+const userExtractor = require('./middleware/userExtractor');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/login', loginRouter);
-// Authorization: populates the request.auth object
-app.use(
-  '/api/blogs',
-  expressjwt({ secret: config.ACCESS_TOKEN, algorithms: ['HS256'] }),
-  blogsRouter
-);
+app.use('/api/blogs', userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 
 app.use(errorHandler);
--- part4/blog/backend/middleware/userExtractor.js
+++ part4/blog/backend/middleware/userExtractor.js
@@ -1,9 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('../utils/config');
 const User = require('../controllers/user');
 
 /**
  * Sets the request.user property.
  */
 module.exports = async (request, response, next) => {
-  request.user = await User.findById(request.auth.id);
+  if (request.cookies.token) {
+    const token = jwt.verify(request.cookies.token, config.ACCESS_TOKEN);
+
+    if (token) {
+      request.user = await User.findById(token.id);
+    }
+  }
+
   next();
 };
